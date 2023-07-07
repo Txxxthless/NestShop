a589c77bf8a7eaf8d6b088cafb87fb61c67c0415
Txxxthless <lepash85@gmail.com>
2023-07-07 11:44:23 +0300

    HomePage created

---
+++ client-app/src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HomeImage from "../assets/homepage.jpg";
+
+export function HomePage() {
+  return (
+    <div className="row" style={{ position: "relative" }}>
+      <img
+        src={HomeImage}
+        alt="Image"
+        className="shadow-lg"
+        style={{ padding: "0px" }}
+      />
+      <h1
+        style={{
+          marginLeft: "25%",
+          position: "absolute",
+          top: "25%",
+          fontSize: "4em",
+          color: "white",
+        }}
+      >
+        Wellcome to NestShop!
+      </h1>
+    </div>
+  );
+}
