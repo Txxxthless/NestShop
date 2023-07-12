ba54b084c66e3f7c614ba42729b602e2e2ee84ad
Txxxthless <lepash85@gmail.com>
2023-07-12 15:17:14 +0300

    Basic basket component template

---
+++ client-app/src/components/Basket.js
@@ -0,0 +1,14 @@
+import { observer } from "mobx-react-lite";
+
+export const Basket = observer(() => {
+    return (
+        <div className="row">
+            <div className="col-3">
+                <h1>Basket</h1>
+            </div>
+            <div className="col-9">
+                
+            </div>
+        </div>
+    )
+});
--- client-app/src/components/Navbar.js
+++ client-app/src/components/Navbar.js
@@ -1,30 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { ShopContext } from "./context/context";
+import { accountApi } from "../http/accountApi";
 
 export const Navbar = observer(() => {
   const { user } = useContext(ShopContext);
 
+  const [isDropdownShown, setIsDropdownShown] = useState(false);
+
+  const logout = () => {
+    accountApi.logout();
+    user.setUser(null);
+  };
+
   return (
-    <header className="d-flex flex-row align-items-center justify-content-center p-3 px-4 mb-3 bg-white border-bottom fixed-top shadow-sm">
-      <nav className="my-2 text-uppercase fs-4">
+    <header className="p-3 px-4 mb-3 bg-white border-bottom fixed-top shadow-sm">
+      <nav
+        className="my-2 text-uppercase fs-4 d-flex justify-content-between"
+        style={{ marginLeft: "30%", marginRight: "30%" }}
+      >
         <Link to="/" className="p-2">
           Home
         </Link>
         {!user.user ? (
-          <>
+          <div>
             <Link to="/login" className="p-2">
               Sign in
             </Link>
             <Link to="/register" className="p-2">
               Register
             </Link>
-          </>
+          </div>
         ) : (
-          <Link to="/account" className="p-2">
-            Account
-          </Link>
+          <div className="dropdown">
+            <Link
+              className="p-2"
+              onClick={() => setIsDropdownShown((value) => !value)}
+            >
+              Account
+            </Link>
+            <ul
+              class={isDropdownShown ? "dropdown-menu show" : "dropdown-menu"}
+            >
+              <li>
+                <h6 style={{ marginLeft: "10%" }}>Account menu</h6>
+                <Link to="/basket" class="dropdown-item mt-4">
+                  View basket
+                </Link>
+              </li>
+              <li>
+                <Link onClick={() => logout()} class="dropdown-item">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
         )}
         <Link to="/shop" className="p-2">
           Shop
